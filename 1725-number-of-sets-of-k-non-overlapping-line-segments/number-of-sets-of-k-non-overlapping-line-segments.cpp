94f383928c7e80ba1442e2a84781cec217d9d647
Gitika <dhingragitika11@gmail.com>
2026-09-16 14:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (100.00%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numberOfSets(int n, int k) {
+        long long MOD = 1e9 + 7;
+        long long N = n + k - 1;
+        long long R = 2 * k;
+
+        if (R > N) return 0;
+
+        auto power = [&](long long base, long long exp) {
+            long long res = 1;
+            base %= MOD;
+            while (exp > 0) {
+                if (exp % 2 == 1) res = (res * base) % MOD;
+                base = (base * base) % MOD;
+                exp /= 2;
+            }
+            return res;
+        };
+
+        auto modInverse = [&](long long num) {
+            return power(num, MOD - 2);
+        };
+
+        long long num = 1, den = 1;
+        for (long long i = 0; i < R; i++) {
+            num = (num * (N - i)) % MOD;
+            den = (den * (i + 1)) % MOD;
+        }
+
+        return (num * modInverse(den)) % MOD;
+    }
+};
