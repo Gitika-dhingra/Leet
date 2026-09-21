283563c1bf78fd8e7597717a5b8fd5b36ed1e676
Gitika <dhingragitika11@gmail.com>
2026-09-21 10:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (98.57%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num ==0 ) return 0;
+        int sum =0;
+        while(num != 0 ){
+            sum = sum + num % 10;
+            num = num/10;
+        }
+        if(sum<=9){
+            return sum;}
+        else {
+        return addDigits(sum); 
+        }
+    } 
+
+};
